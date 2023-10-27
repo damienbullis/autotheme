6224298a4c443908e6582d2b4549bcdf7e534c9b
Damien <80228708+damienbullis@users.noreply.github.com>
2023-10-26 21:40:26 -0500

    little clean up to root command

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"autotheme/pkg/config"
 	"autotheme/pkg/core/harmony"
 	c "autotheme/pkg/utils"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -18,32 +17,15 @@ import (
 const TAB = "    "
 
 var rootCmd = &cobra.Command{
-	Use:   "autotheme",
-	Short: "A zero-config CSS theme generator",
-	Long:  `AutoTheme is a zero-config theme generator using color theory, sensible options & defaults, and modern HTML and CSS features.`,
-
+	Use:     "autotheme",
+	Short:   "A zero-config CSS theme generator",
+	Long:    `AutoTheme is a zero-config theme generator using color theory, sensible options & defaults, and modern HTML and CSS features.`,
+	Aliases: []string{"auto"},
 	Args: func(cmd *cobra.Command, args []string) error {
-		// REFACTOR: Move this to a separate function
-		// check color flag
-		if viper.GetString("color") != "" {
-			clr := viper.GetString("color")
-			len := utf8.RuneCountInString(clr)
-			if (len != 7 && len != 4) || clr[0] != '#' {
-				return errors.New("color flag is invalid, please provide a valid hex code (e.g. #000000 or #000)")
-			}
-		}
-
-		// check harmony flag
-		if viper.GetString("harmony") != "" {
-			// check if harmony flag is valid
-			if viper.GetString("harmony") != "complementary" &&
-				viper.GetString("harmony") != "analogous" &&
-				viper.GetString("harmony") != "triadic" &&
-				viper.GetString("harmony") != "split-complementary" &&
-				viper.GetString("harmony") != "rectangle" &&
-				viper.GetString("harmony") != "" {
-				return errors.New("harmony flag is invalid")
-			}
+		if err := config.CheckColorFlag(); err != nil {
+			return err
+		} else if err := config.CheckHarmonyFlag(); err != nil {
+			return err
 		}
 
 		return nil
@@ -73,29 +55,32 @@ var rootCmd = &cobra.Command{
 		// Build shades / tints / tones / offWB
 		for i, color := range colors {
 			h, s, _ := color.Hsl()
-			offW := colorful.Hsl(h, s, 0.9667)
-			offB := colorful.Hsl(h, s, 0.0333)
 			title := "\nColor " + strconv.Itoa(i+1) + " : " + color.Hex() + "     \n"
 			fmt.Println(title)
 			titleLen := utf8.RuneCountInString(title)
-			shadesStr := "Shades "
-			var _len = 0
+
+			// Calculate shades / tints / tones / offWB
 			shades := c.CalcShades(color, 5)
+			tones := c.CalcTones(color, 5)
+			tints := c.CalcTints(color, 5)
+			offW := colorful.Hsl(h, s, 0.9667)
+			offB := colorful.Hsl(h, s, 0.0333)
+
+			// Printing
+			var _len = 0
+
+			shadesStr := "Shades "
 			_len = utf8.RuneCountInString(shadesStr)
 			for i := 0; i < (titleLen - _len); i++ {
 				shadesStr += "-"
 			}
 			shadesStr += " "
 			for _, shade := range shades {
-				// cw := " W: " + strconv.FormatFloat(c.ContrastRatio(shade, offW), 'f', 2, 64) + " "
-				// cb := "B: " + strconv.FormatFloat(c.ContrastRatio(shade, offB), 'f', 2, 64) + " "
 				shadesStr += colorStr(shade)
 			}
-
 			fmt.Println(shadesStr)
 
 			tonesStr := "Tones "
-			tones := c.CalcTones(color, 5)
 			_len = utf8.RuneCountInString(tonesStr)
 			for i := 0; i < (titleLen - _len); i++ {
 				tonesStr += "-"
@@ -108,7 +93,6 @@ var rootCmd = &cobra.Command{
 
 			tintsStr := "Tints "
 			_len = utf8.RuneCountInString(tintsStr)
-			tints := c.CalcTints(color, 5)
 			for i := 0; i < (titleLen - _len); i++ {
 				tintsStr += "-"
 			}
@@ -123,13 +107,8 @@ var rootCmd = &cobra.Command{
 			for i := 0; i < (titleLen - _len); i++ {
 				offWBStr += "-"
 			}
-			offWBStr += " "
-
-			offWBStr += colorStr(offW)
-			offWBStr += colorStr(offB)
-
+			offWBStr += " " + colorStr(offW) + colorStr(offB)
 			fmt.Println(offWBStr)
-
 		}
 
 		// NEXT: Write theme to file
