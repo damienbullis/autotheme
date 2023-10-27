5a7b94e91dde4e040d37724dc508227c7cc1820a
Damien <80228708+damienbullis@users.noreply.github.com>
2023-10-26 21:41:13 -0500

    add harmony enum

---
+++ pkg/core/harmony/main.go
@@ -0,0 +1,17 @@
+package harmony
+
+const (
+	// Main Harmony
+	Complementary      = "complementary"
+	Analogous          = "analogous"
+	Triadic            = "triadic"
+	Tetratic           = "tetratic"
+	SplitComplementary = "split-complementary"
+	Rectangle          = "rectangle"
+	Square             = "square"
+	// Creative
+	BiPolar      = "bi-polar"
+	Aurelian     = "aurelian"
+	LunarEclipse = "lunar-eclipse"
+	Retrograde   = "retrograde"
+)
