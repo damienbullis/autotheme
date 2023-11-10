760c64f9607583d3ecc68cb90d2d6f248c141412
Damien <80228708+damienbullis@users.noreply.github.com>
2023-11-10 09:28:59 -0600

    finishes generating spacing

--- pkg/core/spacing.go
+++ pkg/core/spacing.go
@@ -3,10 +3,36 @@ package core
 import (
 	"autotheme/pkg/config"
 	"fmt"
+	"math"
 )
 
+type Spacing struct {
+	Scale float64
+}
+
 // GenerateSpacing generates the spacing for the theme
-func GenerateSpacing(config *config.Config) error {
-	fmt.Println("» Spacing: Not implemented yet")
-	return nil
+func GenerateSpacing(config *config.Config) [8]*Spacing {
+	scalar := config.Scalar
+	if scalar == 0.0 {
+		fmt.Println("No scalar provided, using default...")
+		scalar = (1 + math.Sqrt(5)) / 2
+	}
+
+	fmt.Println("\n» Scalar: " + fmt.Sprintf("%f", scalar) + "\n")
+	const root = 16.0
+
+	spacing := [8]*Spacing{
+		{Scale: root / scalar},
+		{Scale: root},
+	}
+
+	for i := 2; i < 8; i++ {
+		spacing[i] = &Spacing{Scale: root * calcScale(scalar, i-1)}
+	}
+
+	return spacing
+}
+
+func calcScale(scalar float64, power int) float64 {
+	return math.Pow(scalar, float64(power))
 }
