bd5ef28f5bd25bd74b792159a50ecc583cdf7e47
Damien <80228708+damienbullis@users.noreply.github.com>
2023-11-25 17:28:11 -0600

    Refactor interactive prompt function

--- pkg/interactive/prompt.go
+++ pkg/interactive/prompt.go
@@ -1,19 +1,105 @@
 package interactive
 
-import "fmt"
+import (
+	"autotheme/pkg/config"
+	"autotheme/pkg/constants"
+	"autotheme/pkg/core/harmony"
+	"autotheme/pkg/utils"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/lipgloss"
+)
 
 func Prompt() {
 	// Prompt user for color
-	fmt.Print("Enter a color for AutoTheme to use: ")
+	fmt.Printf(
+		"\n%s %s ",
+		"Please enter a color:",
+		utils.FgStr("grey", "(or enter to use a random color)"),
+	)
 	var color string
 	_, err := fmt.Scanln(&color)
-	if err != nil {
-		fmt.Println("Please enter a valid hex color code (e.g. #000000 or #000)")
-		return
+	if err != nil && err.Error() != "unexpected newline" {
+		utils.Log.Error("Error reading input: %s", err)
+		os.Exit(1)
+	}
+	if color == "" {
+		fmt.Print(utils.FgStr("grey", "...picking you a color..."))
+		color = utils.GetRandomColor()
+
+		// Prompt user to confirm random color
+		fmt.Printf("\nUse %s as your color? %s ", lipgloss.NewStyle().Foreground(lipgloss.Color(color)).Render(color), utils.FgStr("grey", "(y/n)"))
+		var confirm string
+		_, err := fmt.Scanln(&confirm)
+		if err != nil && err.Error() != "unexpected newline" {
+			utils.Log.Error("Error reading input: %s", err)
+			os.Exit(1)
+		}
+		if confirm != "y" && confirm != "Y" && confirm != "yes" && confirm != "" {
+			os.Exit(0)
+		}
+	} else if config.CheckColorFlag(color) != nil {
+		utils.Log.Error("Invalid color provided")
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", utils.FgStr("green", constants.IconCheck.Str()))
+	// Prompt user for harmony
+	fmt.Printf(
+		"%s %s ",
+		"Please enter a harmony:",
+		utils.FgStr("grey", "(or enter to use a random harmony)"),
+	)
+
+	var harmonyInput string
+	_, err = fmt.Scanln(&harmonyInput)
+	if err != nil && err.Error() != "unexpected newline" {
+		utils.Log.Error("Error reading input: %s", err)
+		os.Exit(1)
+	}
+	if harmonyInput == "" {
+		fmt.Print(utils.FgStr("grey", "\r...picking a harmony..."))
+		harmonyInput = harmony.GetRandomHarmony()
+	} else if config.CheckHarmonyFlag(harmonyInput) != nil {
+		utils.Log.Error("Invalid harmony provided")
+		os.Exit(1)
 	}
 
-	// Prompt user for harmony
-	fmt.Print("Enter a harmony for AutoTheme to use: ")
+	// Confirm harmony
+	fmt.Printf("\nUse %s as your harmony? %s ", harmonyInput, utils.FgStr("grey", "(y/n)"))
+	var confirm string
+	_, err = fmt.Scanln(&confirm)
+	if err != nil && err.Error() != "unexpected newline" {
+		utils.Log.Error("Error reading input: %s", err)
+		os.Exit(1)
+	}
+
+	if confirm != "y" && confirm != "Y" && confirm != "yes" && confirm != "" {
+		os.Exit(0)
+	}
+	fmt.Printf("%s\n", utils.FgStr("green", constants.IconCheck.Str()))
+
+	// Prompt user for darkmode
+	fmt.Printf(
+		"%s %s ",
+		"Using dark mode?",
+		utils.FgStr("grey", "(y/n)"),
+	)
+	var darkmode string
+	_, err = fmt.Scanln(&darkmode)
+	if err != nil && err.Error() != "unexpected newline" {
+		utils.Log.Error("Error reading input: %s", err)
+		os.Exit(1)
+	}
+	if darkmode != "y" && darkmode != "Y" && darkmode != "yes" && darkmode != "" {
+		darkmode = "false"
+	} else {
+		darkmode = "true"
+	}
+	fmt.Printf("%s\n", utils.FgStr("green", constants.IconCheck.Str()))
+
+	//
+
 	// NEXT: Add in harmony options
 
 	// Prompt user for output
