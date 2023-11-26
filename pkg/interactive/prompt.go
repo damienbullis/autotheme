0a069447e5d36d28a1120c1fc1bd1a666bfa680c
Damien <80228708+damienbullis@users.noreply.github.com>
2023-11-26 16:34:54 -0600

    Refactor prompt function and remove unused imports

--- pkg/interactive/prompt.go
+++ pkg/interactive/prompt.go
@@ -1,9 +1,7 @@
 package interactive
 
 import (
-	"autotheme/pkg/config"
 	"autotheme/pkg/constants"
-	"autotheme/pkg/core/harmony"
 	"autotheme/pkg/utils"
 	"fmt"
 	"os"
@@ -13,39 +11,25 @@ import (
 )
 
 func Prompt() {
-	// Prompt user for color
-	utils.Log.Info(
-		"%s %s ",
-		"Please enter a color:",
-		utils.FgStr("grey", "(or enter to use a random color)"),
-	)
-	var color string
-
-	// Read answer
-	_, err := fmt.Scanln(&color)
-	if err != nil && err.Error() != "unexpected newline" {
-		utils.Log.Error("Error reading input: %s", err)
-		os.Exit(1)
-	}
-
-	if color == "" {
-		color, err = PromptColor()
-		if err != nil {
-			utils.Log.Error("Error prompting for color: %s", err)
-			os.Exit(1)
-		}
-	} else if config.CheckColorFlag(color) != nil {
-		utils.Log.Error("Invalid color provided")
+	color, err := ColorPrompt()
+	if err != nil {
+		utils.Log.Error("%s\n", err)
 		os.Exit(1)
 	}
 	clr, _ := colorful.Hex(color)
 	utils.Log.Info(utils.Str("%s %s\n", &clr, nil), color, constants.IconCheck.Str())
 
-	// Prompt user for harmony
-	harmony, err := PromptHarmony()
+	// Check new Harmony prompt
+	harmony, err := HarmonyPrompt()
 	if err != nil {
-		utils.Log.Error("Error prompting for harmony: %s", err)
-		os.Exit(1)
+		if err.Error() == "nevermind" {
+			clearScreen()
+			utils.Log.Info("\nExiting...\n")
+			os.Exit(0)
+		} else {
+			utils.Log.Error("\n%s\n", err)
+			os.Exit(1)
+		}
 	}
 	utils.Log.Info(utils.Str("%s %s\n", &clr, nil), harmony, constants.IconCheck.Str())
 
@@ -73,8 +57,6 @@ func Prompt() {
 		constants.IconCheck.Str(),
 	)
 
-	//
-
 	// NEXT: Add in harmony options
 
 	// Prompt user for output
@@ -84,70 +66,6 @@ func Prompt() {
 	// NEXT: Generate config file
 }
 
-func PromptColor() (string, error) {
-	color := utils.GetRandomColor()
-	cstr, err := colorful.Hex(color)
-	if err != nil {
-		utils.Log.Error("Error converting color to hex: %s", err)
-		os.Exit(1)
-	}
-
-	// Prompt user to confirm random color
-	utils.Log.Info(
-		"Use %s as your color? %s ",
-		utils.Str(color, &cstr, nil),
-		utils.FgStr("grey", "(y/n)"),
-	)
-	var confirm string
-
-	_, err = fmt.Scanln(&confirm)
-	if err != nil && err.Error() != "unexpected newline" {
-		utils.Log.Error("Error reading input: %s", err)
-		os.Exit(1)
-	}
-	if confirm == "y" || confirm == "Y" || confirm == "yes" || confirm == "" {
-		return color, nil
-	}
-	if confirm == "n" || confirm == "N" || confirm == "no" {
-		return PromptColor()
-	}
-	return "", nil
-}
-
-func PromptHarmony() (string, error) {
-	utils.Log.Info(
-		"%s %s ",
-		"Please enter a harmony:",
-		utils.FgStr("grey", "(or enter to use a random harmony)"),
-	)
-
-	var harmonyInput string
-	_, err := fmt.Scanln(&harmonyInput)
-	if err != nil && err.Error() != "unexpected newline" {
-		utils.Log.Error("Error reading input: %s", err)
-		os.Exit(1)
-	}
-	if harmonyInput == "" {
-		harmonyInput = harmony.GetRandomHarmony()
-	} else if config.CheckHarmonyFlag(harmonyInput) != nil {
-		utils.Log.Error("Invalid harmony provided")
-		os.Exit(1)
-	}
-
-	// Confirm harmony
-	utils.Log.Info("Use %s as your harmony? %s ", harmonyInput, utils.FgStr("grey", "(y/n)"))
-	var confirm string
-	_, err = fmt.Scanln(&confirm)
-	if err != nil && err.Error() != "unexpected newline" {
-		utils.Log.Error("Error reading input: %s", err)
-		os.Exit(1)
-	}
-
-	if confirm == "y" || confirm == "Y" || confirm == "yes" || confirm == "" {
-		return harmonyInput, nil
-	}
-	if confirm == "n" || confirm == "N" || confirm == "no" {
-		return PromptHarmony()
-	}
-	return "", nil
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
 }
