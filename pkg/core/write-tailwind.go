39efd72f5e1ba499dcc86311b02357b35f0f9d21
Damien <80228708+damienbullis@users.noreply.github.com>
2023-12-15 18:47:32 -0600

    start tailwind integration

---
+++ pkg/core/write-tailwind.go
@@ -0,0 +1,40 @@
+package core
+
+import "fmt"
+
+func WriteTailwind(args ...any) {
+	fmt.Printf("Not implemented yet %T\n", args)
+
+	// TODO: Write tailwind config
+
+	// Wrapper
+	// /** @type {import('tailwindcss').Config} */
+	// module.exports = {
+	// 		...
+	// }
+	/*
+
+			Theme
+			theme: {
+				... FEATURE: Add theme
+			},
+
+			Sizing
+			spacing: {
+				... FEATURE: Add spacing
+			}
+
+			Darkmode
+			darkMode: ['class', '<config.Prefix>-dark'],
+
+			Noise? not sure... FEATURE: Add noise to tailwind config
+			Gradients? not sure... FEATURE: Add gradients to tailwind config
+
+			module.exports = {
+	  // ...
+	}
+
+
+	*/
+
+}
