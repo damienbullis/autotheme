bd31b2b65287e0b6d097b8b662cea8ca9195593a
Damien <80228708+damienbullis@users.noreply.github.com>
2023-12-16 10:00:41 -0600

    Remove unused code related to color and gradient types

--- pkg/config/init.go
+++ pkg/config/init.go
@@ -9,67 +9,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-type ColorKeys string
-
-const (
-	Primary ColorKeys = "primary"
-	Accent1 ColorKeys = "accent1"
-	Accent2 ColorKeys = "accent2"
-	Accent3 ColorKeys = "accent3"
-	Accent4 ColorKeys = "accent4"
-	Accent5 ColorKeys = "accent5"
-)
-
-// Interface for color types
-type ColorsI interface {
-	// Marker
-	IsColor()
-}
-type ColorsBool bool
-
-func (b ColorsBool) IsColor() {}
-
-type ColorsT map[ColorKeys]bool
-
-func (t ColorsT) IsColor() {}
-
-// Interface for gradient types
-type GradientI interface {
-	// Marker
-	IsGradient()
-}
-type GradientBool bool
-
-func (b GradientBool) IsGradient() {}
-
-type GradientTuple struct {
-	First  ColorKeys
-	Second ColorKeys
-}
-type GradientArray []GradientTuple
-
-func (a GradientArray) IsGradient() {}
-
-type UseClassesI interface {
-	// Marker
-	IsUseClasses()
-}
-
-// Boolean type for use-classes flag
-type UseClassesBool bool
-
-func (b UseClassesBool) IsUseClasses() {}
-
-// Struct for use-classes flag
-type UseClassesT struct {
-	Colors    ColorsI
-	Spacing   bool
-	Noise     bool
-	Gradients GradientI
-}
-
-func (t UseClassesT) IsUseClasses() {}
-
 type Config struct {
 	Primary    string
 	Harmony    string
