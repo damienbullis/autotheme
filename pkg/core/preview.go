24eac566d84c3c09c25b64293be7713156e750aa
Damien <80228708+damienbullis@users.noreply.github.com>
2024-09-28 10:40:37 -0500

    Refactor preview functionality and remove autotheme.html

--- pkg/core/preview.go
+++ pkg/core/preview.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	c "autotheme/pkg/config"
+	"autotheme/pkg/constants"
 	"autotheme/pkg/utils"
 	"path/filepath"
 )
@@ -10,7 +11,7 @@ func WritePreview(config c.Config, palette Palette, scale []float64) {
 
 	if config.Preview {
 		indexHtml := ""
-		utils.Log.Error("Finish building index.html")
+		utils.Log.Error("TODO: Finish building index.html")
 
 		buildIndexHtml(&indexHtml, palette, config)
 		writeIndexHtml(indexHtml, config)
@@ -20,7 +21,7 @@ func WritePreview(config c.Config, palette Palette, scale []float64) {
 func writeIndexHtml(indexHtml string, config c.Config) {
 	path, _ := filepath.Split(config.Output)
 
-	if err := writeFile(filepath.Join(path, "autotheme.html"), indexHtml); err != nil {
+	if err := utils.WriteFile(filepath.Join(path, "autotheme.html"), indexHtml); err != nil {
 		utils.Log.Error(err.Error())
 	} else {
 		utils.Log.Info("Wrote index.html")
@@ -28,6 +29,15 @@ func writeIndexHtml(indexHtml string, config c.Config) {
 }
 
 func buildIndexHtml(htmlString *string, palette Palette, config c.Config) {
-	utils.Log.Error("Finish building index.html")
+	utils.Log.Error("TODO: Finish building index.html")
 	*htmlString += `<!DOCTYPE html>`
+}
+
+func RemovePreview(config c.Config) {
+	path, _ := filepath.Split(config.Output)
+	if result, err := utils.RemoveFile(filepath.Join(path, "autotheme.html")); err != nil {
+		utils.Log.Error(err.Error())
+	} else if result {
+		utils.Log.Info("\nautotheme.html " + utils.FgStr("grey", "removed... ") + constants.IconCheck.Str())
+	}
 }
